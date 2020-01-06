194a04072fc657010f496422679131d652eeb648
chaotao guo <2823321716@qq.com>
2020-01-06 10:21:04 +0800

    创建项目httpserver.c

---
+++ httpserver.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<string.h>
+#include<sys/types.h>
+#include<sys/sockets.h>
+#include<netinet.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <ctype.h>
+#include <sys/stat.h>
+#include <pthread.h>
+
+`
